d64d2369d9c574756b040f0b054eb0f28e43aa2b
Johnathan James <haxwell@gmail.com>
2022-04-13 15:41:38 -0600

    added error handling to login.service/login. also corrected url path.

--- projects/savvato-javascript-services/src/lib/_services/login/login.service.ts
+++ projects/savvato-javascript-services/src/lib/_services/login/login.service.ts
@@ -26,15 +26,19 @@ export class LoginService {
     let data = {email: email, password: password}
 
     return new Promise((resolve, reject) => {
-      this._apiService.postUnsecuredAPI_w_body(environment['apiUrl'] + "/api/login", data)
-          .subscribe((response) => {
+      this._apiService.postUnsecuredAPI_w_body(environment['apiUrl'] + "/api/public/login", data)
+          .subscribe({next: (response) => {
             let o : any = response.body;
 
             self._authService.setUser(new User(o['id'], o['name'], o['password'], o['phone'], o['email']))
             self._authService.setToken(response.headers.get('Authorization') || '')
 
             resolve(response);
-          })
+          }, error: (error) => {
+            console.log("login service error")
+            console.log(error)
+            reject(error);
+          }})
     });
   }
 
