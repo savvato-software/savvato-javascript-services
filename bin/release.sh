198a701fae59cf42a29b0900ef860d644ffaadad
Johnathan James <haxwell@gmail.com>
2023-06-03 16:22:47 -0600

    made the release script smarter

--- bin/release.sh
+++ bin/release.sh
@@ -11,6 +11,6 @@ fi
 # Restoring package*.json files
 find . \( -path "./node_modules" -prune \) -o \( -path "./projects/*" -name "package*.json" -type f -exec git restore -- {} + \)
 
-rm dist/ -rf && ng build savvato-javascript-services && cd projects/savvato-javascript-services/ && npm version patch && cd - && cd dist/savvato-javascript-services/ && npm pack && cd - && date
+rm dist/ -rf && cd projects/savvato-javascript-services/ && npm version patch && cd - && ng build savvato-javascript-services && cd dist/savvato-javascript-services/ && npm pack && cd - && date
 
 cd ~/src/savvato-javascript-services/ && cd dist/savvato-javascript-services && npm publish
